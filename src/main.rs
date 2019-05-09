074f85c0233b2882fb29ffff796cc16a6367d888
Wai Pai Lee <pailee.wai@gmail.com>
2019-05-09 22:21:03 +0800

    Get pixels data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::Write;
+
+use image;
+use image::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut raw_data_file = File::create("./picture_raw.txt").unwrap();
+    let img = image::open("./original.jpg").unwrap();
+
+    for (x, y, data) in img.pixels() {
+        let rgb = data.to_rgb();
+        let [r, g, b] = rgb.data;
+
+        write!(raw_data_file, "{} {} {} ", r, g, b).unwrap();
+
+        if x == 0 && y != 0 {
+            writeln!(raw_data_file, "").unwrap();
+        }
+    }
 }
