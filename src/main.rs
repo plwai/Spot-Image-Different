235c36b9cea214ada758e33ed5d65b420b642cdb
Wai Pai Lee <pailee.wai@gmail.com>
2019-05-28 20:56:00 +0800

    Fix vector option

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use spot_difference::picture::Picture;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let mut picture_1 = Picture::load(args.get(1).to_string()).unwrap();
-    let mut picture_2 = Picture::load(args.get(2).to_string()).unwrap();
+
+    let mut picture_1 = Picture::load(args.get(1).unwrap().to_string()).unwrap();
+    let mut picture_2 = Picture::load(args.get(2).unwrap().to_string()).unwrap();
 
     picture_1.spot_different_default_config(&mut picture_2);
 }
